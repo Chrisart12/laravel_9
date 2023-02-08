7dee8d858eca4463aa0270e22ca62fa192d83182
Issa <chrisissifou@gmail.com>
2023-02-08 21:45:29 +0100

    Initialisation du projet

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+
+import { createApp } from 'vue';
+import AppComponent from './components/AppComponent.vue'
+// import router from './router'
+
+// createApp(AppComponent).use(router).mount('#app')
+createApp(AppComponent).mount('#app')
